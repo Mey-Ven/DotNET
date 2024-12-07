7404d9a5b9ae8fc7c11074121d217db471494764
Mehdi Rahaoui <mehdirahaoui@Windows.local>
2024-12-07 16:21:20 +0100

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static byte[] Buffer {  get; set; }
+        static Socket sck;
+        static void Main(string[] args)
+        {
+            sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sck.Bind(new IPEndPoint(IPAddress.Any, 1234));
+            sck.Listen(100);
+
+            Socket accepted = sck.Accept();
+            Buffer = new byte[accepted.SendBufferSize];
+            int bytesread = accepted.Receive(Buffer);
+            byte[] formatted = new byte[bytesread];
+            for(int i = 0; i < bytesread; i++)
+            {
+                formatted[i] = Buffer[i];
+            }
+            string strData = Encoding .ASCII.GetString(formatted);
+            Console.Write(strData + "\r\n");
+            Console.Read();
+            sck.Close();
+            accepted.Close();
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SocketClient
+{
+    class Program
+    {
+        static Socket sck;
+
+        static void Main(string[] args)
+        {
+            Console.Write("Ecrivez l'adresse IP: ");
+            string ipadd = Console.ReadLine();
+
+            sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Parse(ipadd), 1234);
+
+            try
+            {
+                sck.Connect(localEndPoint);
+            }
+            catch
+            {
+                Console.Write("La connexion est impossible pour l'instant.\r\n");
+                Main(args);
+            }
+            Console.Write("Ecrivez un message: ");
+            string text = Console.ReadLine();
+            byte[] data = Encoding.ASCII.GetBytes(text);
+            sck.Send(data);
+
+            Console.Write("Données envoyées !");
+            Console.Read();
+            sck.Close();
+        }
+    }
+}
